0b8a75a013db9d3c9e7a70f43d7b01593ef3e8d7
Mohtasim Abbasi <62949408+mohtasim70@users.noreply.github.com>
2020-09-26 03:24:55 +0500

    Update temp.go

--- temp.go
+++ temp.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	var i float32
-	j := "mueed-dev"
+	j := "moh-dev"
 	i = 47
 	fmt.Printf("Hello, playground\n")
 	fmt.Printf("%v, %T\n", i, i)
@@ -15,7 +15,30 @@ func main() {
 
 	var numbers = make([]int, 3, 5)
 	printSlice(numbers)
+
+	var fruitArr [2]string
+	arr2 := [3]string{"App", "aaa", "Graaape"}
+	fruitArr[0] = "Apple"
+
+	fmt.Println(arr2[1:2])
+	for i := 1; i <= 10; i++ {
+		//	fmt.Print(i)
+	}
+	//Mapss
+	emails := map[string]string{"Bob": "mai;@hhh", "Opo": "Opo@gmail.com"}
+	fmt.Println(emails["Bob"])
+
+	////Range
+	ids := []int{22, 33, 4, 5, 2, 33, 55}
+
+	for _, id := range ids {
+		fmt.Printf("ID: %d", id)
+	}
+	//Range with map[]type
+	for k, v := range emails {
+		fmt.Printf("%s: %s\n", k, v)
+	}
 }
 func printSlice(x []int) {
-	fmt.Printf("len=%d cap=%d slice=%v\n", len(x), cap(x), x)
+	//	fmt.Printf("len=%d cap=%d slice=%v\n", len(x), cap(x), x)
 }
